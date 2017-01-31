5110a200ebe15e8b0adfed506bc847bd9894ad63
maksota123 <maksota123@mail.ru>
2017-01-31 15:02:29 +0200

    spec1

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+
+
+
+RSpec.describe Task, type: :model do
+  before(:all) do
+    @task = Task.new(title: "my new title")
+  end
+  it 'we got what we need?' do
+    expect(@task.title).to eq("my new title")
+  end
+  it 'validations' do
+    task = FactoryGirl.create(:task)
+    task.title.should == "James Bond"
+  end
+  it 'is not valid with a title longer than 100 symbols' do
+    task = Task.new(title: 'a' * 101)
+end
+end
